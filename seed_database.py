b0b21c788d26155bff1b12f97040645b401ae64c
niwei822 <cecily.li.822@gmail.com>
2022-12-08 08:10:47 -0800

    part 2

---
+++ seed_database.py
@@ -0,0 +1,55 @@
+"""Script to seed database."""
+
+import os
+import json
+from random import choice, randint
+from datetime import datetime
+
+import crud
+import model
+import server
+
+os.system("dropdb ratings")
+# More code will go here
+os.system('createdb ratings')
+model.connect_to_db(server.app)
+model.db.create_all()
+with open('data/movies.json') as f:
+    movie_data = json.loads(f.read())
+    
+# Create movies, store them in list so we can use them
+# to create fake ratings later
+movies_in_db = []
+for movie in movie_data:
+    # TODO: get the title, overview, and poster_path from the movie
+    title, overview, poster_path = (
+        movie["title"],
+        movie["overview"],
+        movie["poster_path"],
+    )
+    release_date = datetime.strptime(movie["release_date"], "%Y-%m-%d")
+    db_movie = crud.create_movie(title, overview, release_date, poster_path)
+    movies_in_db.append(db_movie)
+model.db.session.add_all(movies_in_db)
+model.db.session.commit()
+    # dictionary. Then, get the release_date and convert it to a
+    # datetime object with datetime.strptime
+
+    # TODO: create a movie here and append it to movies_in_db
+for n in range(10):
+    email = f'user{n}@test.com'  # Voila! A unique email!
+    password = 'test'
+
+    # TODO: create a user here
+    user = crud.create_user(email, password)
+    model.db.session.add(user)
+
+    # TODO: create 10 ratings for the user
+for _ in range(10):
+        random_movie = choice(movies_in_db)
+        score = randint(1, 5)
+
+        rating = crud.create_rating(user, random_movie, score)
+        model.db.session.add(rating)
+
+model.db.session.commit()
